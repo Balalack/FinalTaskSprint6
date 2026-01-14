4287d57bc7249c8baed0826787d2503b35a51139
Sergey <balalack@yandex.ru>
2026-01-15 00:25:24 +0300

    first-commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	server := server.NewServer(logger)
+
+	logger.Println("Server started on :8080")
+	if err := server.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatal(err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"fmt"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Conv(text string) (string, error) {
+
+	// Переменная для подсчёта вхождений символов
+	// азбуки Морзе в строку
+	var count int
+	// количество рун в строке
+	var size int
+
+	for i, simv := range text {
+		if simv == '-' || simv == '.' || simv == ' ' {
+			count += 1
+		}
+		size = i
+	}
+
+	if size == 0 {
+		return "", fmt.Errorf("Строка для конвертации не должна быть пустой!")
+	}
+	// return morse.ToMorse(text), nil
+	// Если количество рун в строке совпало с количеством символов
+	// азбуки Морзе в строке, значит эта строка написана в Морзе
+	if size+1 == count {
+		return morse.ToText(text), nil
+	} else {
+		return morse.ToMorse(text), nil
+	}
+
+}
